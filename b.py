c959799d8ec62a0e4fc02475b584df6b8f258c21
Scott Mansfield <scott.mansfield@gmail.com>
2023-02-17 16:57:43 -0500

    initial commit of bib project

---
+++ b.py
@@ -0,0 +1,14 @@
+import string
+import io
+import re
+import urllib.request
+
+file = open('rfc.list', 'r')
+for i in file.readlines():
+    btname = "https://datatracker.ietf.org/doc/%s/bibtex/" % (i.strip())
+    with urllib.request.urlopen(btname) as response:
+        text = response.read().decode('utf-8')
+        print( btname)
+        print( text)
+
+print( 'done\n')
